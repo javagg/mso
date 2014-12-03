5e73bd124da575bed94e4b5535a35416af4015c4
alex <alex@localhost.localdomain>
2014-12-03 21:45:59 +0800

    first commit

---
+++ Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Mso
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseServices(services =>
+				{
+					services.AddSignalR(options =>
+						{
+							options.Hubs.EnableDetailedErrors = true;
+							// options.Hubs.RequireAuthentication();
+						});
+				});
+			app.UseFileServer();
+			app.UseSignalR<RawConnection>("/raw-connection");
+			app.UseSignalR();
+		}
+	}
+}
