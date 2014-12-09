ea667cb9ccc229b35d8c5a783b550bc0513547cb
alex <alex@localhost.localdomain>
2014-12-09 21:50:03 +0800

    add signalr2

---
+++ src/Mso.4.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Mso.MonoDevelop;
+
+namespace Mso
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Service.Initialize();
+            string port = Environment.GetEnvironmentVariable("PORT");
+            port = string.IsNullOrEmpty(port) ? "6118" : port;
+            string url = string.Format("http://*:{0}", port);
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("The Server URL is: {0}", url);
+                Console.ReadLine();
+//                while (true)
+//                    Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ src/Mso.4.0/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using Microsoft.Owin.FileSystems;
+
+[assembly: OwinStartup(typeof(Mso.Startup))]
+
+namespace Mso
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var options = new StaticFileOptions { FileSystem = new PhysicalFileSystem("../../wwwroot") };
+            app.UseStaticFiles(options);
+            app.UseWelcomePage();
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableJSONP = true
+                };
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
